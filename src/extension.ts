3a0130c5dc153b7e9e020bf60e5dbb49bd31e3ce
Brandon Pickering <brandonpickering95@gmail.com>
2020-12-09 17:58:07 -0800

    Refresh on save

--- src/extension.ts
+++ src/extension.ts
@@ -18,43 +18,57 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
+  const inputUri = vscode.Uri.parse("deviz-input-text:/input");
+  const outputUri = vscode.Uri.parse("deviz-output-text:/output");
+
+  const refreshOutput = () => {
+    const inputDocument = vscode.workspace.textDocuments.find(
+      (document) => document.uri.scheme === "deviz-input-text"
+    );
+    if (!inputDocument) {
+      vscode.window.showErrorMessage("Deviz: Must open input pane");
+      return;
+    }
+    const inputText = inputDocument.getText();
+
+    if (!vscode.workspace.workspaceFolders) {
+      vscode.window.showErrorMessage("Deviz: Must have folder open");
+      return;
+    }
+    const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const command = "cargo run";
+
+    const process = spawn(command, { cwd: workspacePath, shell: true });
+
+    process.stdin.write(inputText);
+    process.stdin.end();
+
+    let stdout = "";
+    let numChunks = 0;
+    process.stdout.on("data", (chunk) => {
+      numChunks += 1;
+      stdout += chunk;
+    });
+
+    let stderr = "";
+    process.stderr.on("data", (chunk) => {
+      stderr += chunk;
+    });
+
+    process.on("close", (code) => {
+      console.log(`${command} exited with code ${code}`);
+
+      virtualTextContentProvider.setFileContent(
+        outputUri,
+        `stdout:\n${stdout}\nstderr:\n${stderr}\n`
+      );
+    });
+  };
+
   vscode.workspace.onDidChangeTextDocument((event) => {
     switch (event.document.uri.scheme) {
       case "deviz-input-text":
-        const inputText = event.document.getText();
-
-        if (!vscode.workspace.workspaceFolders) {
-          vscode.window.showErrorMessage("Deviz: Must have folder open");
-          break;
-        }
-        const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        const command = "cargo run";
-
-        const process = spawn(command, { cwd: workspacePath, shell: true });
-
-        process.stdin.write(inputText);
-        process.stdin.end();
-
-        let stdout = "";
-        let numChunks = 0;
-        process.stdout.on("data", (chunk) => {
-          numChunks += 1;
-          stdout += chunk;
-        });
-
-        let stderr = "";
-        process.stderr.on("data", (chunk) => {
-          stderr += chunk;
-        });
-
-        process.on("close", (code) => {
-          console.log(`${command} exited with code ${code}`);
-
-          virtualTextContentProvider.setFileContent(
-            vscode.Uri.parse("deviz-output-text:/output"),
-            `stdout:\n${stdout}\nstderr:\n${stderr}\n`
-          );
-        });
+        refreshOutput();
         break;
 
       case "deviz-output-text":
@@ -66,24 +80,20 @@ export function activate(context: vscode.ExtensionContext) {
         break;
     }
   });
+  vscode.workspace.onDidSaveTextDocument(refreshOutput);
 
   context.subscriptions.push(
     vscode.commands.registerCommand("deviz.show", async () => {
-      await vscode.window.showTextDocument(
-        vscode.Uri.parse("deviz-input-text:/input"),
-        {
-          viewColumn: vscode.ViewColumn.Two,
-          preview: false,
-        }
-      );
-      await vscode.window.showTextDocument(
-        vscode.Uri.parse("deviz-output-text:/output"),
-        {
-          viewColumn: vscode.ViewColumn.Two,
-          preserveFocus: true,
-          preview: false,
-        }
-      );
+      await vscode.window.showTextDocument(inputUri, {
+        viewColumn: vscode.ViewColumn.Two,
+        preserveFocus: true,
+        preview: false,
+      });
+      await vscode.window.showTextDocument(outputUri, {
+        viewColumn: vscode.ViewColumn.Two,
+        preserveFocus: true,
+        preview: false,
+      });
     })
   );
 }
