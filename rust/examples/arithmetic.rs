3be49ae48b8407c4906fef63f67ccc1a54cbf38e
Brandon Pickering <brandonpickering95@gmail.com>
2020-12-20 18:18:14 -0800

    Cleanup

--- rust/examples/arithmetic.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut graph = deviz::graph("g");
-    graph.node_labeled("root", "ROOT");
-    graph.node("A");
-    graph.node("B");
-    graph.edge("root", "A");
-    graph.edge_labeled("root", "B", "edge label");
-}
